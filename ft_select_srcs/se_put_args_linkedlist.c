351bf9d4c7caca954fd090f9dac85c911ac9d1d7
Jere Rignell <jrignell@student.hive.fi>
2020-05-13 15:54:58 +0300

    deleted ls funtion

--- ft_select_srcs/se_put_args_linkedlist.c
+++ ft_select_srcs/se_put_args_linkedlist.c
@@ -6,7 +6,7 @@
 /*   By: jrignell <jrignell@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/13 12:03:05 by jrignell          #+#    #+#             */
-/*   Updated: 2020/05/13 13:56:20 by jrignell         ###   ########.fr       */
+/*   Updated: 2020/05/13 15:54:21 by jrignell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,29 +40,3 @@ void	se_put_args_linkedlist(t_sh *t)
 	t->head = node;
 	((t_args *)t->head->content)->cursor = TRUE;
 }
-
-/*
-	void		ls_lstadd_linkedlist(t_list **node, t_ls *f, char *s, int i)
-	{
-		t_list		*new_node;
-		t_file		content;
-		struct stat	buf;
-
-		if (lstat(s, &buf) == -1)
-			ls_error(s);
-		ft_bzero(&content, sizeof(t_file));
-		f->num_of_nodes++;
-		content.path = s;
-		content.name = i ? s : ret_name(s);
-		if (f->max_name_len < ((int)ft_strlen(i ? s : (ret_name(s)))))
-			f->max_name_len = ft_strlen(i ? s : (ret_name(s)));
-		ls_type_mode(&buf, &content);
-		if (f->l)
-			ls_get_data_l_flag(f, &content, &buf);
-		if (f->t)
-			content.epoc = buf.st_mtime;
-		new_node = ft_lstnew(&content, sizeof(t_file));
-		ft_lstadd(node, new_node);
-	}
-
-*/
